856fa0110e691e3216ad831da3aad693786a207d
Soikat Rahman <hello.dev.sr@gmail.com>
2022-01-04 22:33:15 +0600

    rooms: admin v2

--- rooms/admin.py
+++ rooms/admin.py
@@ -9,12 +9,12 @@ class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
-    # list_display = ("name", "used_by",)
+    list_display = ("name", "used_by",)
 
-    # def used_by(self, obj):
-    #     return obj.rooms.count()
+    def used_by(self, obj):
+        return obj.rooms.count()
 
-    pass
+    # pass
 
 
 # class PhotoInline(admin.TabularInline):
@@ -29,4 +29,64 @@ class PhotoAdmin(admin.ModelAdmin):
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+        "count_photos",
+        "total_rating",
+    )
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+    search_fields = ("=city", "^host__username")
+    filter_horizontal = ("amenities", "facilities", "house_rules")
+    fieldsets = (
+        (
+            "Basic Info",
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "country",
+                    "city",
+                    "address",
+                    "price",
+                    "room_type",
+                )
+            },
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {"fields": ("amenities", "facilities", "house_rules")},
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
+    count_amenities.short_description = "Amenity Count"
+
+    def count_photos(self, obj):
+        return obj.photos.count()
+
+    count_photos.short_description = "Photo Count"
