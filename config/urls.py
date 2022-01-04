0295f9e63871ad0b5df2d655fd98a65229efc5ec
Soikat Rahman <hello.dev.sr@gmail.com>
2022-01-05 00:45:01 +0600

    template init

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path('', include('rooms.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def rooms(request):
+    return render(request, "rooms/index.html", {'value': 'Hello World'})
