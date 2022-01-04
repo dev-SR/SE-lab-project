a9296507da46939a4ec736a921540f4e4179a9c4
Soikat Rahman <hello.dev.sr@gmail.com>
2022-01-05 01:01:19 +0600

    heroku: static files fix v1

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, re_path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rooms.urls')),
+    re_path(r'^', include('app.urls')),
+
+    re_path(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT}),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
